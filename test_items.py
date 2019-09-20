41933125c079185056e27a8cae7c4433523f6052
ArtV1ctory <vikaartemeva98@yandex.ru>
2019-09-20 19:58:03 +0300

    Changed the check for the existence of the button on the page

--- test_items.py
+++ test_items.py
@@ -1,4 +1,4 @@
 def test_button_add_to_basket_works(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-    assert len(browser.find_elements_by_css_selector(".btn-add-to-basket")) > 0, \
+    assert browser.find_element_by_css_selector('.btn-add-to-basket'), \
         'The add to basket button does not exist!'
